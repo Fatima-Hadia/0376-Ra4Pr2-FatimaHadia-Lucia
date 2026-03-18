a35a7000034cefbd079cfe124691272c3f6eb6e3
Hadia Fatima <hfatima25@ilb.cat>
2026-03-18 12:30:40 +0100

    Inicio del proyecto. Archivos generados

---
+++ script.js
@@ -0,0 +1,4 @@
+//Generació de Selects: Crear un bucle que ompli els 4 desplegables amb números del 0 al 9.
+//Funció logTerminal(missatge, tipus): Crear una funció que rebi un text i el tipus (èxit, error o normal) i l'imprimeixi a la zona de terminal del HTML amb un efecte de "línia de comanda".
+//Gestor d'Esdeveniments: Escoltar el clic del botó "Executar Codi", recollir els valors dels 4 selects i cridar a la lògica de comparació.
+//Control de Rondes: Actualitzar el comptador visual de rondes restants a cada intent.
